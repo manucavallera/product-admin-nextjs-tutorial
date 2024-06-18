41fe3753172a1e90a414119b237e07b7c9240826
manucavallera <120131770+manucavallera@users.noreply.github.com>
2024-06-18 20:43:41 -0300

    Fix fileToBase64 to ensure it returns a string, and update DragAndDropImage to handle base64 string correctly

--- actions/convert-file-to-based64.ts
+++ actions/convert-file-to-based64.ts
@@ -1,8 +1,15 @@
-export const fileToBase64 = async (file: File) => {
+export const fileToBase64 = (file: File): Promise<string> => {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
+    reader.onload = () => {
+      const result = reader.result;
+      if (typeof result === "string") {
+        resolve(result);
+      } else {
+        reject(new Error("Failed to convert file to base64 string."));
+      }
+    };
     reader.onerror = (error) => reject(error);
   });
 };
--- components/drag-and-drop-image.tsx
+++ components/drag-and-drop-image.tsx
@@ -1,18 +1,27 @@
 "use client";
 
 import { fileToBase64 } from "@/actions/convert-file-to-based64";
-import { Dropzone, ExtFile, FileMosaic, FileMosaicProps } from "@files-ui/react";
+import {
+  Dropzone,
+  ExtFile,
+  FileMosaic,
+  FileMosaicProps,
+} from "@files-ui/react";
 import * as React from "react";
 
-export default function DragAndDropImage({ handleImage }: { handleImage: (url: string) => void }) {
+interface DragAndDropImageProps {
+  handleImage: (url: string) => void;
+}
+
+export default function DragAndDropImage({
+  handleImage,
+}: DragAndDropImageProps) {
   const [files, setFiles] = React.useState<ExtFile[]>([]);
 
-
   //***Get the file , convert to base 64 and use "handleImage function to update the form" ****/
-  const updateFiles = async(incommingFiles: ExtFile[]) => {
-    
+  const updateFiles = async (incommingFiles: ExtFile[]) => {
     const file = incommingFiles[0].file as File;
-    const base64 = await fileToBase64(file);
+    const base64: string = await fileToBase64(file);
 
     handleImage(base64);
     setFiles(incommingFiles);
@@ -29,8 +38,8 @@ export default function DragAndDropImage({ handleImage }: { handleImage: (url: s
       value={files}
       header={false}
       footer={false}
-      label="Add an image"
-      accept=".webp, .jpg, .jpeg, .png"
+      label='Add an image'
+      accept='.webp, .jpg, .jpeg, .png'
       maxFiles={1}
       minHeight={"135px"}
       //accept="image/*"
@@ -40,7 +49,7 @@ export default function DragAndDropImage({ handleImage }: { handleImage: (url: s
           key={file.id}
           {...file}
           onDelete={removeFile}
-          preview 
+          preview
           resultOnTooltip
           alwaysActive
         />
